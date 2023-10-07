f741d7af7b7646a5fe75067d673aab1cbcad05b7
Stefan Dukic <96357300+UGOI@users.noreply.github.com>
2023-10-07 17:29:59 +0200

    build(increase version): 0.1.9 and change smth

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React lolimnffffffff
+          Learn React lolimnffffffffjnjnnjnnjnjnjn
         </a>
       </header>
     </div>
