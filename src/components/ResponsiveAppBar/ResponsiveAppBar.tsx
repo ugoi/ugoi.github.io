310cf9f48075385a36a1077355bb5ad05d45e6cc
Stefan Dukic <96357300+UGOI@users.noreply.github.com>
2023-10-12 16:59:22 +0200

    feat(AppBar): make AppBar hide on scroll and make it fixed

--- src/components/ResponsiveAppBar/ResponsiveAppBar.tsx
+++ src/components/ResponsiveAppBar/ResponsiveAppBar.tsx
@@ -10,6 +10,32 @@ import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import MenuItem from "@mui/material/MenuItem";
 import { Link } from "react-router-dom"; // Import Link
+import { Slide, useScrollTrigger } from "@mui/material";
+
+interface Props {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window?: () => Window;
+  children: React.ReactElement;
+}
+
+function HideOnScroll(props: Props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+  });
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
 
 interface ResponsiveAppBarProps {
   routes: {
@@ -39,104 +65,106 @@ function ResponsiveAppBar({ routes, logo }: ResponsiveAppBarProps) {
   const rootPath = logo.path;
 
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component={Link} // Use Link component
-            to={rootPath}
-            sx={{
-              flexGrow: 1,
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            Sd.
-          </Typography>
-
-          <Typography
-            variant="h5"
-            noWrap
-            component={Link} // Use Link component
-            to={rootPath}
-            sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            Sd.
-          </Typography>
-          <Box sx={{ flexGrow: 0, display: { xs: "none", md: "flex" } }}>
-            {routes.map((route) => (
-              <Button
-                key={route.path}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: "white", display: "block" }}
-                component={Link}
-                to={route.path}
-              >
-                {route.name}
-              </Button>
-            ))}
-          </Box>
-
-          <Box sx={{ flexGrow: 0, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
+    <HideOnScroll>
+      <AppBar position="fixed">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Typography
+              variant="h6"
+              noWrap
+              component={Link} // Use Link component
+              to={rootPath}
               sx={{
-                display: { xs: "block", md: "none" },
+                flexGrow: 1,
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
               }}
             >
+              Sd.
+            </Typography>
+
+            <Typography
+              variant="h5"
+              noWrap
+              component={Link} // Use Link component
+              to={rootPath}
+              sx={{
+                mr: 2,
+                display: { xs: "flex", md: "none" },
+                flexGrow: 1,
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              Sd.
+            </Typography>
+            <Box sx={{ flexGrow: 0, display: { xs: "none", md: "flex" } }}>
               {routes.map((route) => (
-                <MenuItem
+                <Button
                   key={route.path}
                   onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: "white", display: "block" }}
                   component={Link}
                   to={route.path}
                 >
-                  <Typography textAlign="center">{route.name}</Typography>
-                </MenuItem>
+                  {route.name}
+                </Button>
               ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+            </Box>
+
+            <Box sx={{ flexGrow: 0, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                {routes.map((route) => (
+                  <MenuItem
+                    key={route.path}
+                    onClick={handleCloseNavMenu}
+                    component={Link}
+                    to={route.path}
+                  >
+                    <Typography textAlign="center">{route.name}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </HideOnScroll>
   );
 }
 export default ResponsiveAppBar;
