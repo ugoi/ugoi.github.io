be7cc7b2f6062d1999c4f26781586968c779cc0d
Stefan Dukic <96357300+UGOI@users.noreply.github.com>
2023-10-12 23:12:58 +0200

    feat(portfolio page): improve it add picture

--- src/pages/Portfolio/Portfolio.tsx
+++ src/pages/Portfolio/Portfolio.tsx
@@ -8,63 +8,74 @@ import {
   CardActions,
   Button,
   IconButton,
+  Container,
 } from "@mui/material";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import projects from "./projectsData";
+import WebIcon from "@mui/icons-material/Web";
 
 const Portfolio: React.FC = () => {
   return (
-    <Grid container spacing={3} mt={3}>
-      <Grid item xs={12}>
-        <Typography variant="h4" gutterBottom>
-          My Best Projects
-        </Typography>
-        <Typography variant="subtitle1" gutterBottom>
-          A showcase of my most notable work.
-        </Typography>
-      </Grid>
-      {projects.map((project, index) => (
-        <Grid item xs={12} md={6} key={index}>
-          <Card>
-            <CardMedia
-              component="img"
-              height="140"
-              image={project.imageUrl}
-              alt={project.title}
-            />
-            <CardContent>
-              <Typography variant="h5" component="div">
-                {project.title}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {project.description}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <IconButton
-                edge="start"
-                color="inherit"
-                aria-label="GitHub"
-                href={project.githubLink}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <GitHubIcon />
-              </IconButton>
-              <Button
-                size="small"
-                color="primary"
-                href={project.deployedLink}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                View Deployed
-              </Button>
-            </CardActions>
-          </Card>
+    <Container>
+      <Grid container spacing={3} mt={3}>
+        <Grid item xs={12}>
+          <Typography variant="h4" gutterBottom>
+            My Best Projects
+          </Typography>
+          <Typography variant="subtitle1" gutterBottom>
+            A showcase of my most notable work.
+          </Typography>
         </Grid>
-      ))}
-    </Grid>
+        {projects.map((project, index) => (
+          <Grid item xs={12} sm={6} md={4} key={index}>
+            <Card>
+              <CardMedia
+                component="img"
+                height="250"
+                image={project.imageUrl}
+                alt={project.title}
+                sx={{ objectFit: "contain" }}
+              />
+              <CardContent>
+                <Typography variant="h5" component="div">
+                  {project.title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {project.description}
+                </Typography>
+              </CardContent>
+              <CardActions sx={{ justifyContent: "center" }}>
+                <IconButton
+                  edge="start"
+                  color="inherit"
+                  aria-label="GitHub"
+                  href={project.githubLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <GitHubIcon sx={{ mr: 1 }} />
+                  <Typography variant="body2" component="span">
+                    GitHub
+                  </Typography>
+                </IconButton>
+                <IconButton
+                  edge="start"
+                  color="inherit"
+                  href={project.deployedLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <WebIcon sx={{ mr: 1 }} />
+                  <Typography variant="body2" component="span">
+                    View Deployed
+                  </Typography>
+                </IconButton>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 };
 
--- src/pages/Portfolio/projectsData.ts
+++ src/pages/Portfolio/projectsData.ts
@@ -1,10 +1,12 @@
+import portfolio from "../../ugoi_page.png";
+
 const projects = [
   {
     title: "Portfolio Page",
     description: "My portfolio page",
-    imageUrl: "/path/to/image1.jpg",
-    githubLink: "https://github.com/project1",
-    deployedLink: "https://project1.com",
+    imageUrl: portfolio,
+    githubLink: "https://github.com/ugoi/ugoi.github.io",
+    deployedLink: "https://ugoi.vercel.app/",
   },
   {
     title: "Project 2",
@@ -13,7 +15,13 @@ const projects = [
     githubLink: "https://github.com/project2",
     deployedLink: "https://project2.com",
   },
-  // Add more projects as needed.
+  {
+    title: "Project 2",
+    description: "This is the description for project 2.",
+    imageUrl: "/path/to/image2.jpg",
+    githubLink: "https://github.com/project2",
+    deployedLink: "https://project2.com",
+  },
 ];
 
 export default projects;
