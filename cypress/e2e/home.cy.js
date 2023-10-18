87ea88aea467c62a1480cccf11a5fd070d854cf4
Stefan Dukic <96357300+UGOI@users.noreply.github.com>
2023-10-18 20:09:59 +0200

    test(e2e): add cypress and tests for home

---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,63 @@
+/// <reference types="cypress" />
+
+describe("Home Page", () => {
+  // Visit your homepage before each test
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
+  });
+
+  // Check if the main greetings and your name render correctly
+  it("displays the main greetings and name", () => {
+    cy.contains("Hi There!");
+    cy.contains("I'm Stefan Dukic");
+  });
+
+  // Check if the homeLogo is displayed correctly
+  it("displays the home logo", () => {
+    cy.get('img[alt="home"]').should("be.visible");
+  });
+
+  // Verify the typewriter effect is showing one of its strings
+  it("displays a string from the typewriter effect", () => {
+    cy.contains("Full-Stack Developer").should("be.visible"); // You can adjust to check for any of the strings in the Typewriter
+  });
+
+  it("LinkedIn button contains the correct base LinkedIn URL", () => {
+    // Scroll to the LinkedIn button
+    cy.get('[data-testid="linkedin-button"]')
+      .scrollIntoView()
+      .should("be.visible");
+
+    // Check that the LinkedIn button's href attribute contains the base LinkedIn URL
+    cy.get('[data-testid="linkedin-button"]')
+      .should("have.attr", "href")
+      .and("include", "https://www.linkedin.com/");
+  });
+
+  it("LinkedIn profile URL is valid", () => {
+    // Confirm the URL is valid
+    cy.request({
+      url: "https://www.linkedin.com/in/stefan-dukic-68682b20b/",
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.oneOf([200, 999]);
+    });
+  });
+
+  it("GitHub button contains the correct base GitHub URL", () => {
+    // Scroll to the GitHub button
+    cy.get('[data-testid="github-button"]')
+      .scrollIntoView()
+      .should("be.visible");
+
+    // Check that the GitHub button's href attribute contains the base GitHub URL
+    cy.get('[data-testid="github-button"]')
+      .should("have.attr", "href")
+      .and("include", "https://github.com/");
+  });
+
+  it("GitHub profile URL is valid", () => {
+    // Confirm the URL is valid
+    cy.request("https://github.com/ugoi").its("status").should("eq", 200);
+  });
+});
--- src/pages/Portfolio/Portfolio.test.tsx
+++ src/pages/Portfolio/Portfolio.test.tsx
@@ -32,23 +32,5 @@ describe("Portfolio Component", () => {
         });
       }
     });
-
-    projects.forEach((project, index) => {
-      if (project.githubLink) {
-        test(`GitHub link for project ${index} points to the correct URL`, () => {
-          render(<Portfolio />);
-          const githubLink = screen.getByTestId(`github-link-${index}`);
-          expect(githubLink).toHaveAttribute("href", project.githubLink);
-        });
-      }
-
-      if (project.deployedLink) {
-        test(`Deployed link for project ${index} points to the correct URL`, () => {
-          render(<Portfolio />);
-          const deployedLink = screen.getByTestId(`deployed-link-${index}`);
-          expect(deployedLink).toHaveAttribute("href", project.deployedLink);
-        });
-      }
-    });
   });
 });
