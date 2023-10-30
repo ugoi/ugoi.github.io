c5a8e3b6d8acab361a8ad29673a6a6a647a41a12
Stefan Dukic <96357300+UGOI@users.noreply.github.com>
2023-10-30 10:17:46 +0100

    refactor(Message): rename isBot to isConvnersant

--- src/components/Message/Message.tsx
+++ src/components/Message/Message.tsx
@@ -16,44 +16,44 @@ const Message: React.FC<MessageProps> = ({
 }) => {
   // <-- Added avatarSrc here
   const theme = useTheme();
-  const isBot = direction === "incoming";
+  const isConversant = direction === "incoming";
 
   return (
     <Box
       sx={{
         display: "flex",
-        justifyContent: isBot ? "flex-start" : "flex-end",
+        justifyContent: isConversant ? "flex-start" : "flex-end",
         mb: 2,
       }}
     >
       <Box
         sx={{
           display: "flex",
-          flexDirection: isBot ? "row" : "row-reverse",
+          flexDirection: isConversant ? "row" : "row-reverse",
           alignItems: "center",
         }}
       >
         <Avatar
           src={avatarSrc} // <-- Added this line
           sx={{
-            bgcolor: isBot
+            bgcolor: isConversant
               ? theme.palette.primary.main
               : theme.palette.secondary.main,
           }}
         >
-          {avatarSrc ? null : isBot ? author.charAt(0) : "U"}
+          {avatarSrc ? null : isConversant ? author.charAt(0) : "U"}
         </Avatar>
         <Paper
           variant="outlined"
           sx={{
             p: 2,
-            ml: isBot ? 1 : 0,
-            mr: isBot ? 0 : 1,
-            backgroundColor: isBot
+            ml: isConversant ? 1 : 0,
+            mr: isConversant ? 0 : 1,
+            backgroundColor: isConversant
               ? theme.palette.primary.dark
               : theme.palette.secondary.dark,
             color: theme.palette.text.primary, // Adjusting text color based on theme
-            borderRadius: isBot ? "20px 20px 20px 5px" : "20px 20px 5px 20px",
+            borderRadius: isConversant ? "20px 20px 20px 5px" : "20px 20px 5px 20px",
           }}
         >
           <Typography variant="body1">{text}</Typography>
